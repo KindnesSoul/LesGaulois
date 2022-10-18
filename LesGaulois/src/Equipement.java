3bdcf3a3dba464bf11dddc055d3c8773bf704315
KindnesSoul <114509653+KindnesSoul@users.noreply.github.com>
2022-10-18 15:43:56 +0200

    Add files via upload

---
+++ LesGaulois/src/Equipement.java
@@ -0,0 +1,12 @@
+
+package personnages;
+public enum Equipement {
+	CASQUE("Casque"),BOUCLIER("Bouclier");
+}
+private String nom;
+private Equipement(String nom) {
+	this.nom=nom;
+}
+public string toString{
+	return nom;
+}
